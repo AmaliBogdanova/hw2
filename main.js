5f0f4bf18b8ba61104afc1322d271331f4072d27
amalia <ab14>
2022-10-06 20:06:54 +0500

    finish

---
+++ main.js
@@ -0,0 +1,8 @@
+var x = +prompt('введите число')
+console.log('Первое число: ' + x);
+var y = +prompt('введите число')
+console.log('Второе число: ' + y);
+var z = +prompt('введите число')
+console.log('Третье число: ' + z);
+var res = ((x+y+z)/3)
+console.log('Среднее арифметическое: ' + res);
